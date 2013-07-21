7c5f25f99ff8e3b5b671c7c14745d4d974ff243a
Ivan Gotovchits <ivg@ieee.org>
2013-07-21 19:21:46 +0400

    now Sector heavily uses Angle function.
    And not depends on local opens.

--- sector.ml
+++ sector.ml
@@ -1,26 +1,14 @@
-module A = Angle
-type a = A.t
+open Angle
+type a = Angle.t
 
 type t = a * a
 
-let zero = A.of_radian 0.
-
-
 let create a a' = a,a'
-
-let of_radian (a,a') =
-  let a  = A.of_radian a  in
-  let a' = A.of_radian a' in
-  a,a'
-
-let to_radian (a,a') =
-  let a  = A.to_radian a   in
-  let a' = A.to_radian a'  in
-  a,a'
-
+let of_radian (a,a') = of_radian a, of_radian a'
+let to_radian (a,a') = to_radian a, to_radian a'
 
 let of_bisect ~direct ~length =
-  A.(direct - length / 2), A.(direct + length / 2)
+  direct - length / 2, direct + length / 2
 
 let start (s,_) = s
 let finish (_,f) = f
@@ -29,17 +17,11 @@ let rec contains b = function
   | a, a' when a <= a' -> b >= a && b <= a'
   | a, a' -> not(contains b (a', a)) || b = a' || b = a
 
-let length (a,a') =
-  let diff = A.(a' - a) in
-  if diff < A.null then A.(diff + full) else diff
-
-let radian_of_length s = A.to_radian (length s)
-
-let degree_of_length s = A.to_degree (length s)
-
+let length (a,a') = a' - a
+let radian_of_length s = Angle.to_radian (length s)
+let degree_of_length s = Angle.to_degree (length s)
 let sort (a, a') = if a < a' then a ,a' else a',a
-
-let bisector (a,a') = A.bisector a a'
+let bisector (a,a') = bisector a a'
 let includes s s' = contains (start s) s' && contains (finish s) s'
 let intersects s s' =
   contains (start s) s' || contains (finish s) s' || includes s' s
